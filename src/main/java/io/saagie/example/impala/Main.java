c44d0c29a158ef6eeb1d37baa7094f939a3db33c
Pierre Leresteux <pierre.leresteux@gmail.com>
2016-08-23 21:45:32 +0200

    refacto(log & variable) : Correct usage for logger and for variables

--- src/main/java/io/saagie/example/impala/Main.java
+++ src/main/java/io/saagie/example/impala/Main.java
@@ -3,81 +3,61 @@ package io.saagie.example.impala;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Logger;
 
 public class Main {
 
 
-	private static final Logger logger = Logger.getLogger("io.saagie.example.impala.ReadWrite");
-	private static final String JDBC_DRIVER_NAME = "org.apache.hive.jdbc.HiveDriver";
-	private static String connectionUrl;
+    private static final Logger logger = Logger.getLogger("io.saagie.example.impala.Main");
+    private static final String JDBC_DRIVER_NAME = "org.apache.hive.jdbc.HiveDriver";
+    private static String connectionUrl;
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		if (args.length < 1) {
-			logger.severe("1 arg is required :\n\t- connectionurl ex: jdbc:hive2://impalahost:21050/;auth=noSasl");
-			System.err.println("1 arg is required :\n\t-connectionurl  ex: jdbc:hive2://impalahost:21050/;auth=noSasl");
-			System.exit(128);
-		}
-		// Get url Connection
-		connectionUrl = args[0];
+        if (args.length < 1) {
+            logger.severe("1 arg is required :\n\t- connectionurl ex: jdbc:hive2://impalahost:21050/;auth=noSasl");
+            System.err.println("1 arg is required :\n\t-connectionurl  ex: jdbc:hive2://impalahost:21050/;auth=noSasl");
+            System.exit(128);
+        }
+        // Get url Connection
+        connectionUrl = args[0];
 
-		// Init Connection
-		Connection con = null;
+        // Init Connection
+        Connection con = null;
 
-		//==== Select data from Impala Table
-		String sqlStatement = "SELECT * FROM helloworld";
-		try {
-			// Set JDBC Driver
-			Class.forName(JDBC_DRIVER_NAME);
-			// Connect to Hive/Impala
-			con = DriverManager.getConnection(connectionUrl);
-			// Init Statement
-			Statement stmt = con.createStatement();
-			// Execute Query
-			ResultSet rs = stmt.executeQuery(sqlStatement);
+        //==== Select data from Impala Table
+        String sqlStatement = "SELECT * FROM helloworld";
+        try {
+            // Set JDBC Driver
+            Class.forName(JDBC_DRIVER_NAME);
+            // Connect to Hive/Impala
+            con = DriverManager.getConnection(connectionUrl);
+            // Init Statement
+            Statement stmt = con.createStatement();
+            // Execute Query
+            stmt.executeQuery(sqlStatement);
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				con.close();
-			} catch (Exception e) {
-				// swallow
-			}
-		}
+            logger.info("Select Impala : OK");
 
-		//==== Insert data into Impala Table
-		sqlStatement = "INSERT INTO helloworld2 SELECT * FROM helloworld";
-		try {
-			// Set JDBC Driver
-			Class.forName(JDBC_DRIVER_NAME);
-			// Connect to Hive/Impala
-			con = DriverManager.getConnection(connectionUrl);
-			// Init Statement
-			Statement stmt = con.createStatement();
-			// Execute Query
-			stmt.execute(sqlStatement);
-			// Invalidate metadata to update changes
-			stmt.execute("INVALIDATE METADATA");
+            //==== Insert data into Impala Table
+            sqlStatement = "INSERT INTO helloworld2 SELECT * FROM helloworld";
+            // Execute Query
+            stmt.execute(sqlStatement);
+            logger.info("Insert Impala : OK");
+            // Invalidate metadata to update changes
+            stmt.execute("INVALIDATE METADATA");
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				con.close();
-			} catch (Exception e) {
-				// swallow
-			}
-		}
+        } catch (Exception e) {
+            logger.severe(e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                // swallow
+            }
+        }
 
 
-	}
+    }
 }
